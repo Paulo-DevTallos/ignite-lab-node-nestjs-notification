1a71538f25789773770fd55636e87846d359b58b
Paulo Sergio <pauloserg861@gmail.com>
2022-12-14 20:45:47 -0300

    criando entidades daaplicacao / configurando jest

--- src/notifications.controller.ts
+++ src/notifications.controller.ts
@@ -1,18 +1,19 @@
+/* eslint-disable prettier/prettier */
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { CreateNotificationDto } from './infra/dto/createNotification-dto';
 import { NotificationsService } from './notifications.service';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private readonly notificationService: NotificationsService) {}
+	constructor(private readonly notificationService: NotificationsService) {}
 
-  @Post()
-  async createNotification(@Body() data: CreateNotificationDto) {
-    return await this.notificationService.create(data);
-  }
+	@Post()
+	async createNotification(@Body() data: CreateNotificationDto) {
+		return await this.notificationService.create(data);
+	}
 
-  @Get()
-  listNotifications() {
-    return this.notificationService.list();
-  }
+	@Get()
+	listNotifications() {
+		return this.notificationService.list();
+	}
 }
