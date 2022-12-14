de8b31f0bf837b1fc89c4331a2b7ed00e748b835
Paulo Sergio <pauloserg861@gmail.com>
2022-12-14 15:06:37 -0300

    criando mock de notificacoes

--- src/notifications.controller.ts
+++ src/notifications.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
 
-@Controller()
+@Controller('notifications')
 export class Notifications {
   constructor(private readonly prismaService: PrismaService) {}
 
+  @Post()
+  async create() {
+    await this.prismaService.notification.create({
+      data: {
+        id: randomUUID(), // creating a unique id
+        content: 'Você tem uma nova solicitação de amizade',
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
+  }
+
   @Get()
-  getHello() {
-    return this.prismaService.notificarion.findMany();
+  list() {
+    return this.prismaService.notification.findMany();
   }
 }
